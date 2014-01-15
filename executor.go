8932cdb9a56675965009c0e05dac12b50b67c07b
tobz <tobias.lawrence@gmail.com>
2014-01-14 21:41:29 -0500

    Out with the old, in with the new.  Everything has been reworked to be more readable and more straightforward.  There is no notion of stopping gracefully.  If oldschool dies, you have a small window to restart or you need to resync.  Fuck eventual consistency.  Take what's yours, right now.  Everything passes errors back up the chain, right up to the piece of code that runs the executor.  If it's an error we don't expect, the application dies.  Trying to be cute is what kills you.  Play it cool, and restart that daemon, homie.

--- executor.go
+++ executor.go
@@ -1,32 +1,51 @@
 package oldschool
 
 import "github.com/coreos/go-etcd/etcd"
+import "strconv"
+import "log"
+
+const (
+	EtcdErrEventIndexCleared = 401
+	EtcdErrKeyNotFound       = 100
+)
 
 type Executor struct {
-	etcdClient  *etcd.Client
-	baseDir     string
-	baseEtcdDir string
+	executorName string
+	etcdClient   *etcd.Client
+	baseDir      string
+	baseEtcdDir  string
 }
 
-func NewExecutor(etcdClient *etcd.Client, baseDir string, baseEtcdDir string) *Executor {
+func NewExecutor(executorName string, etcdClient *etcd.Client, baseDir string, baseEtcdDir string) *Executor {
 	return &Executor{
-		etcdClient:  etcdClient,
-		baseDir:     baseDir,
-		baseEtcdDir: baseEtcdDir,
+		executorName: executorName,
+		etcdClient:   etcdClient,
+		baseDir:      baseDir,
+		baseEtcdDir:  baseEtcdDir,
 	}
 }
 
 func (me *Executor) Run() error {
 	// Figure out if we've done any work in the past, and if we can pick it back up.
-	lastProcessedIndex := me.getLastProcessedIndex()
+	lastProcessedIndex, err := me.getLastProcessedIndex()
+	if err != nil {
+		return err
+	}
+
 	if lastProcessedIndex != 0 {
+		log.Printf("[runner] Found indicator of previous work: last processed index set as '%d'", lastProcessedIndex)
+
 		// See if our last processed index is still in etcd's history.
 		presentInHistory := me.checkHistoryForIndex(lastProcessedIndex)
 		if presentInHistory {
+			log.Print("[runner] Found last processed index still in etcd history.  Trying to start from last index...")
+
 			// We should still be able to catch up, so start from our forward position.
 			return me.startFromLastIndex()
 		}
 	}
+
+	log.Print("[runner] No previous indicator of work found.  Starting this node from scratch...")
 
 	// Either we've never done anything or we're too far out of sync, so start from scratch.
 	return me.startFromScratch()
@@ -36,56 +55,128 @@ func (me *Executor) startFromScratch() error {
 	// Set up our channels and guards and what not.
 	events := make(chan *etcd.Response, 128)
 	errors := make(chan error, 1)
-	startProcessing := false
 
 	// First, set up a routine that handles our watcher.
 	go func() {
+		log.Print("[watcher] Starting watch for latest changes...")
+
 		_, err := me.etcdClient.Watch(me.baseEtcdDir, 0, true, events, nil)
 		if err != nil {
+			log.Printf("[watcher] Caught error while watching: %s", err)
+
 			errors <- err
 		}
 	}()
 
-	// Now set up a routine that processes events once we've bulk loaded.
-	go func() {
-		// Block until we're allowed to process.
-		for {
-			if startProcessing {
-				break
-			}
-		}
+	// Now set up our routine that processes events as we receive them.
+	eventProcessor := func() {
+		log.Print("[processor] Starting to process event backlog...")
 
 		// Now listen for and process all events that we get.
 		for {
 			select {
 			case event := <-events:
+				log.Printf("[processor] Received event: %s -> %s (%d / %d / %d)",
+					event.Action, event.Node.Key, event.EtcdIndex, event.RaftIndex, event.RaftTerm)
+
 				err := me.processEvent(event)
 				if err != nil {
+					log.Printf("[processor] Caught error while processing event: %s", err)
+
 					errors <- err
 					break
 				}
 			}
 		}
-	}()
+	}
+
+	log.Print("[bootstrap] Starting bulk load of universe from etcd...")
+
+	// Now we need to bulk load the data and process it.
+	response, err := me.etcdClient.Get(me.baseEtcdDir, false, true)
+	if err != nil {
+		log.Printf("[bootstrap] Caught error while trying to get the universe: %s", err)
+		return err
+	}
+
+	err = me.processEvent(response)
+	if err != nil {
+		log.Printf("[bootstrap] Caught an error while trying to process our bootstrap get: %s", err)
+		return err
+	}
+
+	log.Print("[bootstrap] Starting processor...")
+
+	go eventProcessor()
+
+	// Now just watch for any errors that our routines throw, and spit them back up the chain if we get one.
+	log.Print("[bootstrap] Spinning while we wait for any potential error.  We are now operational and in cruise control mode.")
+
+	for {
+		select {
+		case err = <-errors:
+			log.Printf("[bootstrap] Caught an error from worker routine: %s", err)
+			return err
+		}
+	}
 
 	return nil
 }
 
 func (me *Executor) startFromLastIndex() error {
+	log.Print("[warm restart] Not implemented.")
+
 	return nil
 }
 
 func (me *Executor) processEvent(event *etcd.Response) error {
+	log.Printf("[handler] Processing event: %s -> %s (%d / %d / %d)",
+		event.Action, event.Node.Key, event.EtcdIndex, event.RaftIndex, event.RaftTerm)
+
 	return nil
 }
 
 func (me *Executor) checkHistoryForIndex(index uint64) bool {
-	return false
+	_, err := me.etcdClient.Watch(me.baseEtcdDir, index, true, nil, nil)
+	return errorIsEtcdError(err, EtcdErrEventIndexCleared)
 }
 
-func (me *Executor) getLastProcessedIndex() uint64 {
-	return 0
+func (me *Executor) getLastProcessedIndex() (uint64, error) {
+	// Try and grab whatever we may have put there last.
+	response, err := me.etcdClient.Get(me.baseEtcdDir+"/_executors/_"+me.executorName, false, false)
+	if err != nil {
+		// If we simply couldn't find the key - that's not an issue, per se.  We only care about
+		// failures of the cluster or something.
+		if errorIsEtcdError(err, EtcdErrKeyNotFound) {
+			return 0, nil
+		}
+
+		// Couldn't find anything.  Started from the bottom, now we here.
+		return 0, err
+	}
+
+	// This is the simplest way to convert a string to a uint64.  Ugh.
+	index, err := strconv.ParseUint(response.Node.Value, 10, 64)
+	if err != nil {
+		// I like clearly defined error handling situations, but fuck it.  I'm returning a
+		// zero if something stupid happened and we set this to a non-numeric value.
+		return 0, err
+	}
+
+	return index, nil
 }
 
-func (me *Executor) setLastProcessedIndex(index uint64) {
+func (me *Executor) setLastProcessedIndex(index uint64) error {
+	_, err := me.etcdClient.Set(me.baseEtcdDir+"/_executors/_"+me.executorName, strconv.FormatUint(index, 10), 0)
+	return err
+}
+
+func errorIsEtcdError(err error, errCode int) bool {
+	if err == nil {
+		return false
+	}
+
+	// If the error code we're checking for is -1, we're just trying to match an EtcdError, but not a specific one.
+	etcdErr, ok := err.(*etcd.EtcdError)
+	return ok && (etcdErr.ErrorCode == errCode || errCode == -1)
 }
--- oldschool/main.go
+++ oldschool/main.go
@@ -1,47 +1,42 @@
 package main
 
-import "os"
 import "flag"
 import "log"
 import "strings"
-import "os/signal"
 import "github.com/coreos/go-etcd/etcd"
 import "github.com/tobz/oldschool"
 
 var (
-	joinNode    = flag.String("join-node", "127.0.0.1:4001", "the address of one of the etcd nodes in the cluster")
-	baseDir     = flag.String("base-dir", "", "the base directory to synchronize files within")
-	baseEtcdDir = flag.String("base-etcd-dir", "", "the base directory to watch in etcd")
+	executorName = flag.String("name", "", "the name of this agent (must be unique)")
+	joinNode     = flag.String("join-node", "127.0.0.1:4001", "the address of one of the etcd nodes in the cluster")
+	baseDir      = flag.String("base-dir", "", "the base directory to synchronize files within")
+	baseEtcdDir  = flag.String("base-etcd-dir", "", "the base directory to watch in etcd")
 )
 
 func init() {
 	flag.Parse()
 
+	if *executorName == "" {
+		log.Fatal("[main] You must specify a name for this agent and it must be unique.  If you try and pass in a non-unique name, we'll find out, and simply exit.  Make your move.")
+	}
+
 	if *baseDir == "" || *baseEtcdDir == "" {
-		log.Fatal("You must specify both a base etcd directory to synchronize and the base directory on the filesystem to synchronize it to!")
+		log.Fatal("[main] You must specify both a base etcd directory to synchronize and the base directory on the filesystem to synchronize it to!")
 	}
 }
 
 func main() {
+	// Set etcd to debug mode for all da info.
+	etcd.OpenDebug()
+
 	// Create our etcd client and make sure we can sync to the cluster.
 	etcdClient := etcd.NewClient(strings.Split(*joinNode, ","))
 	if !etcdClient.SyncCluster() {
-		log.Fatal("Couldn't synchronize with the etcd cluster! (Is it down? Is your join node accurate?)")
+		log.Fatal("[main] Couldn't synchronize with the etcd cluster! (Is it down? Is your join node accurate?)")
 	}
 
 	// Now create our change executor.  This guy keeps track of and waits for changes, and then executes them
 	// by turning the key into a file path, mirroring the keys on disk.
-	executor := oldschool.NewExecutor(etcdClient, *baseDir, *baseEtcdDir)
-	executor.Start()
-
-	// Wait to be closed or until we close ourselves.
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	for {
-		select {
-		case <-c:
-			executor.Stop()
-			os.Exit(1)
-		}
-	}
+	executor := oldschool.NewExecutor(*executorName, etcdClient, *baseDir, *baseEtcdDir)
+	log.Fatal(executor.Run())
 }
